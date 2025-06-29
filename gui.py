b1ad25fe0d1a563a25d1aef13d3c94b3908becb6
Keivan <keivan.razban@gmail.com>
2025-06-29 14:07:20 +0200

    mathematical filter and mouse clicking to add cells

--- gui.py
+++ gui.py
@@ -4,9 +4,9 @@ from PIL import Image, ImageTk
 from itertools import combinations
 from utils import *
 
-xmax = 300
-ymax = 300
-cell_size = 3
+xmax = 100
+ymax = 100
+cell_size = 5
 dt = 1  # milliseconds
 
 combs = list(combinations(list(range(8)),2))
@@ -26,12 +26,29 @@ shape = np.array([
     [0,0,0,0,0,0,0,0,0,0],
 ])
 
-#shape = np.stack([shape]*3, -1)
+shape = np.array([
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,0],
+    [0,0,0,0,0,0,1,0,0,0],
+    [0,0,0,1,1,1,1,0,0,1],
+    [0,0,0,1,0,0,1,0,0,1],
+    [0,0,0,1,1,1,1,1,1,1],
+    [0,0,1,1,1,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+])
+
+def gen_shape():
+    return np.random.choice(2,(20,20))
+shape1 = gen_shape()
+shape2 = gen_shape()
+shape3 = gen_shape()
 
 # Add the shape at various positions
-cells[xmax//2-5:xmax//2+5,ymax//2-5:ymax//2+5,0] = shape
-cells[xmax//3-5:xmax//3+5,ymax//2-5:ymax//2+5,1] = shape
-cells[xmax//3-5:xmax//3+5,ymax//5-5:ymax//5+5,2] = shape
+cells[xmax//2:xmax//2+shape1.shape[0],ymax//2:ymax//2+shape1.shape[1],0] = shape1
+cells[xmax//3:xmax//3+shape2.shape[0],ymax//2:ymax//2+shape2.shape[1],1] = shape2
+cells[xmax//3:xmax//3+shape3.shape[0],ymax//3:ymax//3+shape3.shape[1],2] = shape3
 # cells[xmax//4-5:xmax//4+5,ymax//5-5:ymax//5+5] = shape
 # cells[xmax//5-5:xmax//5+5,ymax//5-5:ymax//5+5] = shape
 
@@ -40,22 +57,47 @@ fen_princ = tk.Tk()
 fen_princ.title("GAME OF LIFE")
 
 monCanvas = tk.Canvas(fen_princ, width=xmax*cell_size, height=ymax*cell_size, bg='ivory')
+monCanvas.pack()
 
-def update_cells(cells):
-    return rules(cells)
+def update_cells():
+    global cells
+    cells = rules(cells)
 
-def update_canvas(cells):
+def update_canvas():
+    global cells
     img =  ImageTk.PhotoImage(image=Image.fromarray((cells*255).astype(np.uint8)).resize((xmax*cell_size, ymax*cell_size), Image.NEAREST))
     monCanvas.delete("all")
     monCanvas.pack()
     monCanvas.create_image(0,0, anchor="nw", image=img)
     monCanvas.img = img  # Keep a reference to prevent garbage collection
 
-def update(cells):
-    cells = update_cells(cells)
-    update_canvas(cells)
-    monCanvas.after(dt, update, cells)
+def update():
+    global cells
+    update_cells()
+    update_canvas()
+    monCanvas.after(dt, update)
 
-update(cells)
+r = 6
+def click_red(event):
+    global cells
+    y = event.x//cell_size
+    x = event.y//cell_size
+    cells[x-r:x+r, y-r:y+r, 0] = 1
+def click_green(event):
+    global cells
+    y = event.x//cell_size
+    x = event.y//cell_size
+    cells[x-r:x+r, y-r:y+r, 1] = 1
+def click_blue(event):
+    global cells
+    y = event.x//cell_size
+    x = event.y//cell_size
+    cells[x-r:x+r, y-r:y+r, 2] = 1
+
+monCanvas.bind('<Button-1>', click_red)
+monCanvas.bind('<Button-2>', click_green)
+monCanvas.bind('<Button-3>', click_blue)
+
+update()
 
 fen_princ.mainloop()
--- utils.py
+++ utils.py
@@ -1,10 +1,36 @@
 import numpy as np
 from scipy import signal
+from PIL import Image, ImageTk
+
+def gaussian_filter(dimx, dimy, mu, std):
+        pos = np.stack([
+            np.repeat(
+                np.expand_dims(np.arange(dimx),1),
+                dimy,
+                1
+            ),
+            np.repeat(
+                np.expand_dims(np.arange(dimy),0),
+                dimx,
+                0
+            ),
+        ], -1)
+        values = np.exp(-np.sum((pos-mu)**2,-1)/std)
+        return values
+    
+def doughnut_filter(dimx, dimy, std):
+    mu = (dimx/2, dimy/2)
+    surround = gaussian_filter(dimx, dimy, mu, std)
+    heart = gaussian_filter(dimx, dimy, mu, std/5)
+    doughnut = surround - heart
+    return doughnut/np.sum(doughnut)
+
+filt = doughnut_filter(20,20,10)
 
 def rules(cells):
     # 1+6, 3+4 -> square
     # 2+5, 0+7 -> hexagone
-    ur = 1e-2
+    ur = 1e-1
     # sumcells = np.zeros((cells.shape[0]-2,cells.shape[1]-2, 3))
     # sumcells += cells[2:,2:] #0
     # sumcells += cells[2:,1:-1] #1
@@ -15,36 +41,25 @@ def rules(cells):
     # sumcells += cells[:-2,1:-1] #6
     # sumcells += cells[:-2,:-2] #7
 
-    filt = np.array([
-        [0,0,1,1,0,0],
-        [0,1,1,1,1,0],
-        [1,1,0,0,1,1],
-        [1,1,0,0,1,1],
-        [0,1,1,1,1,0],
-        [0,0,1,1,0,0],
-    ])
+
+    
+    # img = Image.fromarray((filt/2*255).astype(np.uint8))
+    # img.show()
+    # input()
+
     sumcells = np.stack([
         signal.convolve2d(cells[:,:,0], filt, boundary='symm', mode='same'),
         signal.convolve2d(cells[:,:,1], filt, boundary='symm', mode='same'),
         signal.convolve2d(cells[:,:,2], filt, boundary='symm', mode='same'),
         ],-1)
 
-
-
-    cells[:,:,0] += ur*diff_func(sumcells[:,:,1])#-ur*gaussian(sumcells[:,:,2], mu=3,std=5,amp=5,h=.8)
-    cells[:,:,1] += ur*gaussian(sumcells[:,:,2], mu=3.1,std=5,amp=5,h=.8)#-ur*gaussian(sumcells[:,:,0], mu=3.1,std=5,amp=5,h=.8)
-    cells[:,:,2] += ur*gaussian(sumcells[:,:,0], mu=3.2,std=5,amp=5,h=.8)#-ur*gaussian(sumcells[:,:,1], mu=3.2,std=5,amp=5,h=.8)
+    alpha = 10
+    cells[:,:,0] += ur*gaussian(sumcells[:,:,0], mu=0.75,std=.1,amp=5,h=.7)+ur*alpha*np.maximum(0,sumcells[:,:,0]*sumcells[:,:,1]-.1)-ur*alpha*np.maximum(0,sumcells[:,:,2]-.3)
+    cells[:,:,1] += ur*gaussian(sumcells[:,:,1], mu=0.75,std=.1,amp=5,h=.7)+ur*alpha*np.maximum(0,sumcells[:,:,1]*sumcells[:,:,2]-.1)-ur*alpha*np.maximum(0,sumcells[:,:,0]-.3)
+    cells[:,:,2] += ur*gaussian(sumcells[:,:,2], mu=0.75,std=.1,amp=5,h=.7)+ur*alpha*np.maximum(0,sumcells[:,:,2]*sumcells[:,:,0]-.1)-ur*alpha*np.maximum(0,sumcells[:,:,1]-.3)
     cells = cells.clip(0,1)
 
     return cells
 
 def gaussian(x, mu=3,std=5,amp=5,h=.8):
     return amp * (np.exp(-(x-mu)**2/std)+h-1)
-
-
-def diff_func(x):
-    mu = 3
-    std = 5
-    amp = 5
-    h = .8
-    return gaussian(x, mu,std,amp,h)
